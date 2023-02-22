07796d480f04e600440d8286cdf4ae8334028f18
Caio <Caio@DESKTOP-2TQO33J>
2023-02-21 22:10:43 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        Decimal valor1 = 0, valor2 = 0;
+        String operacao = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "1";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "3";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "4";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "5";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "9";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + ".";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            valor2 = Decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+
+            if(operacao == "SOMA")
+            {
+                txtResultado.Text = Convert.ToString(valor1 + valor2);
+            }else if(operacao == "SUBTRACAO")
+            {
+                txtResultado.Text = Convert.ToString(valor1 - valor2);
+            }else if(operacao == "MULTIPLICACAO")
+            {
+                txtResultado.Text = Convert.ToString(valor1 * valor2);
+            }else if(operacao == "DIVISAO")
+            {
+                txtResultado.Text = Convert.ToString(valor1 / valor2);
+            }
+        
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = Decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "SUBTRACAO";
+                labelOperacao.Text = "-";
+            }
+            else
+            {
+                MessageBox.Show("Informe um valor");
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = Decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "MULTIPLICACAO";
+                labelOperacao.Text = "x";
+            }
+            else
+            {
+                MessageBox.Show("Informe um valor");
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = Decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "DIVISAO";
+                labelOperacao.Text = "/";
+            }
+            else
+            {
+                MessageBox.Show("Informe um valor");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+            labelOperacao.Text = "";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+            valor1 = 0;
+            valor2 = 0;
+            labelOperacao.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = Decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "SOMA";
+                labelOperacao.Text = "+";
+            }
+            else
+            {
+                MessageBox.Show("Informe um valor");
+            }
+        }
+    }
+}
